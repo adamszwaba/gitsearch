c008a2a0187ca6150247d53c9fb4a2a06aaed78e
Adam Szwaba <adam.szwaba@setapp.pl>
2021-03-18 11:23:54 +0100

    Remove irrelevant theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,15 +8,13 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+} from "@chakra-ui/react";
+import { Logo } from "./Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
           <Text>
@@ -35,4 +33,4 @@ export const App = () => (
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
