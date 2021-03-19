6850de300c15e1c8f19f71401befcebbe24f1b26
Adam Szwaba <adam.szwaba@setapp.pl>
2021-03-19 20:03:03 +0100

    Add skeleton, remove roboto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import 'fontsource-roboto';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
