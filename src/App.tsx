5e876e7f858ba7f047ceb865db3dedce0a323e53
Adam Szwaba <adam.szwaba@setapp.pl>
2021-03-19 16:27:31 +0100

    Add top layer styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,97 @@
 import * as React from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Button,
+  ChakraProvider,
+  Container,
+  extendTheme,
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react';
-import { Logo } from './Logo';
 
-export const MyApp: React.FC = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-);
+import { SearchIcon } from '@chakra-ui/icons';
+
+const theme = extendTheme({
+  fonts: {
+    body: `Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    heading: `Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    mono: `Roboto, SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
+  },
+  styles: {
+    Input: {
+      variants: {
+        filled: {
+          backgroundColor: '#F0F3F4',
+        },
+      },
+    },
+    global: {
+      body: {
+        '-webkit-font-smoothing': 'antialiased',
+        '-moz-osx-font-smoothing': 'grayscale',
+        fontSize: '16px',
+      },
+      html: {
+        backgroundColor: '#FBFCFD',
+      },
+    },
+  },
+  colors: {
+    main: '#452CDC',
+  },
+  shadows: {
+    base: '0px 2px 7px rgba(0, 0, 0, 0.1)',
+  },
+});
+
+export const MyApp: React.FC = () => {
+  console.log(theme);
+  return (
+    <ChakraProvider theme={theme}>
+      <Box shadow="base">
+        <Container padding="6" height="84px">
+          <FormControl display="flex" flexDirection="row">
+            <InputGroup>
+              <InputLeftElement pointerEvents="none" height="9" width="8">
+                <SearchIcon color="#4F4F4F" w="15.71px" h="15.71px" />
+              </InputLeftElement>
+              <Input
+                height="9"
+                borderRadius="lg"
+                fontSize="sm"
+                lineHeight="8"
+                paddingLeft="8"
+                variant="filled"
+                type="text"
+                placeholder="Search for users"
+              />
+            </InputGroup>
+            <Button
+              fontStyle="normal"
+              minWidth="16"
+              height="9"
+              padding="3"
+              marginLeft="4"
+              fontWeight="bold"
+              fontSize="12px"
+              lineHeight="12px"
+              letterSpacing="0.4px"
+              borderRadius="lg"
+              backgroundColor="main"
+              colorScheme="main"
+              type="submit"
+              isLoading={false}
+              loadingText="Searching"
+            >
+              Search
+            </Button>
+          </FormControl>
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
+};
 
 export default MyApp;
