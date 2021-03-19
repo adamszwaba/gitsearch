c9fbe8137ed4751b0475c05d61d95bde23c03ed6
Adam Szwaba <adam.szwaba@setapp.pl>
2021-03-19 16:27:09 +0100

    Add fontsource

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontsource-roboto';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
